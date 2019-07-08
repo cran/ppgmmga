dff8397ecfce9595d7b91ffaf8d9410277cde1c8
Alessio Serafini <srf.alessio@gmail.com>
2019-07-08 08:10:03 +0000

    version 1.2

--- inst/doc/ppgmmga.R
+++ inst/doc/ppgmmga.R
@@ -1,11 +1,12 @@
 ## ----setup, include=FALSE------------------------------------------------
 library(knitr)
 opts_chunk$set(fig.align = "center", 
-               out.width = "90%",
+               out.width = "80%",
                fig.width = 6, fig.height = 5,
-               dev.args=list(pointsize=10),
+               dev.args = list(pointsize = 10),
                par = TRUE, # needed for setting hook 
                collapse = TRUE, # collapse input & ouput code in chunks
+               message = FALSE,
                warning = FALSE)
 
 knit_hooks$set(par = function(before, options, envir)
@@ -17,13 +18,14 @@ knit_hooks$set(par = function(before, options, envir)
 library(ppgmmga)
 cat(ppgmmga:::ppgmmgaStartupMessage(), sep="")
 
-## ---- fig.width = 6, fig.height = 6--------------------------------------
+## ---- fig.width = 7, fig.height = 6, out.width = "100%"------------------
 library(mclust)
 data("banknote")
 X <- banknote[,-1]
 Class <- banknote$Status
 table(Class)
-clPairs(X, classification = Class)
+Class_color <- ggthemes::tableau_color_pal("Classic 10")(2)
+clPairs(X, classification = Class, colors = Class_color)
 
 ## ------------------------------------------------------------------------
 pp1D <- ppgmmga(data = X, d = 1, approx = "UT", seed = 1)
@@ -51,28 +53,34 @@ plot(pp2D)
 plot(pp2D, class = Class, drawAxis = FALSE)
 
 ## ------------------------------------------------------------------------
-gmm <- densityMclust(data = scale(X, center = TRUE, scale = FALSE), G = 2)
 pp3D <- ppgmmga(data = X, d = 3, 
-                center = TRUE, scale = FALSE, gmm = gmm, 
+                center = TRUE, scale = FALSE, 
                 gatype = "gaisl", 
                 options = ppgmmga.options(numIslands = 2),
                 seed = 1)
-summary(pp3D$GA)
+summary(pp3D, check = TRUE)
 
 ## ------------------------------------------------------------------------
 plot(pp3D$GA)
 
-## ---- fig.width = 6, fig.height = 6--------------------------------------
+## ---- fig.width = 7, fig.height = 6, out.width = "100%"------------------
 plot(pp3D)
 
-## ---- fig.width = 6, fig.height = 6--------------------------------------
+## ---- fig.width = 7, fig.height = 6, out.width = "100%"------------------
 plot(pp3D, class = Class)
 
-## ------------------------------------------------------------------------
+## ---- fig.width = 6, fig.height = 5--------------------------------------
 plot(pp3D, dim = c(1,2))
 
-## ---- fig.width = 7, fig.height = 5--------------------------------------
+## ---- fig.width = 7, fig.height = 5, out.width = "90%"-------------------
 plot(pp3D, dim = c(1,3), class = Class)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  # A rotating 3D plot can be obtained using
+#  if(!require("msir")) install.packages("msir")
+#  msir::spinplot(pp3D$Z, markby = Class,
+#                 pch.points = c(19,17),
+#                 col.points = Class_color)
 
 ## ---- echo=FALSE, results='asis'-----------------------------------------
 print(citation("ppgmmga"), style = "text")
